19e32f45e3a550b4420d592e64afefab94c5993b
Achraf Hebboul <adhebboul@gmail.com>
2023-09-04 21:38:36 +0100

    fixing bugs v2

--- resources/views/livewire/operations/operations-list.blade.php
+++ resources/views/livewire/operations/operations-list.blade.php
@@ -7,9 +7,7 @@
           <th scope="col" class="px-6 py-4 font-medium text-gray-900">Observation</th>
           <th scope="col" class="px-6 py-4 font-medium text-gray-900">Date Debut</th>
           <th scope="col" class="px-6 py-4 font-medium text-gray-900">Date Fin</th>
-          @unlessrole('Infirmier')
-            <th scope="col" class="px-6 py-4 font-medium text-gray-900"></th>
-          @endunlessrole
+          <th scope="col" class="px-6 py-4 font-medium text-gray-900"></th>
         </tr>
       </thead>
       <tbody class="divide-y divide-gray-100 border-t border-gray-100">
